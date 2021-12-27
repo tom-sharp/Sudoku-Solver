e3c0af1f6e47ac5e43d6405329e8161f3ef0a4b6
tom-sharp <dink521g@gmail.com>
2021-12-27 19:19:39 +0100

    Changed version number

--- SudokuSolver.cs
+++ SudokuSolver.cs
@@ -18,7 +18,7 @@ namespace Sudoku.Solver
 {
 	public class SudokuSolver
 	{
-		string MSGVersion = "Sudoku-Solver 0.02  (-? for help)";
+		string MSGVersion = "Sudoku-Solver 0.03  (-? for help)";
 		string demopuzzle1 = "38........5...2..6...14....1...8..3...9.3.8.4..2.........62.7....6...........1.8.";
 		string demopuzzle2 = "..4..7...6......5........9...195....29....7..8...1...3.....32.8.5..........12...4";
 		string demopuzzle3 = "6....1.7......75..3.....9...4..9.3.........8....5.4.2..7.6.8....93...7....6.2..1.";
