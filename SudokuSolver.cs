f04077576e353f11753448bdd754b92a10574d71
tom-sharp <dink521g@gmail.com>
2022-01-05 13:18:58 +0100

    version 0.09

--- SudokuSolver.cs
+++ SudokuSolver.cs
@@ -8,54 +8,58 @@ using Syslib;
 using Sudoku.Puzzle;
 
 /*
+ * Solving performance test	solving: 4000-puzzles.txt (located in binaries folder)
+ *			14 seconds using logic only to solve all 4000 puzzles
+ *			20 seconds using backtrack only (-bo) to solve all 4000 puzzles
+ * 
+ * 
  *		Version		Description
- *		0.01		Initial
- *		0.02		End application after showing help if help switch is provided
- *		0.03		Moved SudokuPuzzle to its own assembly
- *		0.04		Added feature to read sudoku puzzle from file
- *		0.05		Fixed problem with demo puzzles introduced in version 0.04
+ *		0.09		Added creation of puzzles (switch -cp)
+ *					Added option to use backtrack only (switch -bo)
+ *		0.08		Added creation of random solved sudoku
+ *					Changed more situations to show puzzle as grid
+ *		0.07		Removed limitation of max 81 sudoko lines read from file and increased file buffer
+ *					Removed Numpass algorithm
  *		0.06		Extracted some code to its own method
  *					Re-arranged how to show help
  *					Added option to show mask and puzzle as grid
  *					Added feature to read multiple puzzles from one file
  *					BugFix - when reading puzzle from file, the opened file was never closed
- *		0.07		Removed limitation of max 81 sudoko lines read from file and increased file buffer
- *					Removed Numpass algorithm
- *		0.08		Added creation of random solved sudoku
- *					Changed more situations to show puzzle as grid
+ *		0.05		Fixed problem with demo puzzles introduced in version 0.04
+ *		0.04		Added feature to read sudoku puzzle from file
+ *		0.03		Moved SudokuPuzzle to its own assembly
+ *		0.02		End application after showing help if help switch is provided
+ *		0.01		Initial
+ *					
  */
 
 namespace Sudoku.Solver
 {
 	public class SudokuSolver
 	{
-		string MSGVersion = "Sudoku-Solver 0.08  (-? for help)";
+		string MSGVersion = "Sudoku-Solver 0.09  (-? for help)";
 		string[] MSGHelp = {
-			"usage: sudoku-solver  {-?} {-r} {-b} {-m} {-1} {-2} {-3} {puzzlestring}",
-			"   By default 3R Algorithm is used (not showing progress)",
+			"usage: sudoku-solver  {-?} {-r} {-b} {-c {count}} {-cp {count}} {-m} {-1} {-2} {-3} {puzzlestring}",
 			"      -? show this help",
 			"      -f read puzzle from file",
 			"      -fr read puzzles from file, each line should be a puzzle",
 			"      -r show progress solving 3R algorithm ",
 			"      -b extend solving using backtrack algorithm",
+			"      -bo use backtrack algorithm only",
 			"      -m show possible numbers mask if not able to solve puzzle",
 			"      -v show puzzle and number mask as a grid",
-			"      -c Create Sudoku solution",
+			"      -c {count} Create Sudoku solution, count = number of sudokus to create",
+			"      -cp {count} Create Sudoku puzzle, count = number of puzzles to create",
 			"      -1 solve demo puzzle 1",
 			"      -2 solve demo puzzle 2",
 			"      -3 solve demo puzzle 3",
-			"   puzzlestring a string representing the sudokupuzzle containing 81 characters",
-			"   of numbers 1-9 and '.' '0' 'x' for undefined cells. If not provided or",
-			"   string is shorter than 81 characters the remaining cells will be set as",
-			"   undefined",
+			"   puzzlestring a string representing the sudokupuzzle containing 81 characters of numbers 1-9 and",
+			"   '.' '0' 'x' for undefined cells. If not provided or string is shorter than 81 characters the",
+			"   remaining cells will be set as undefined",
 			"\n   Examples:",
-			"   sudoku-solver -1 -r",
-			"   sudoku-solver -f puzzle.txt",
-			"   sudoku-solver -f -m puzzle.txt",
-			"   sudoku-solver -f -b puzzle.txt",
-			"   sudoku-solver -fr puzzles.txt",
-			"   sudoku-solver -m \"123456789\"",
-			"   sudoku-solver -b \"123456789\"",
+			"   sudoku-solver -1 -r              sudoku-solver -m \"123456789\"     sudoku-solver -cp 10",
+			"   sudoku-solver -f puzzle.txt      sudoku-solver -b \"123456789\"     sudoku-solver -cp -v",
+			"   sudoku-solver -f -m puzzle.txt   sudoku-solver -f -b puzzle.txt   sudoku-solver -fr puzzles.txt",
 		};
 
 		public void ShowHelp() {
@@ -74,11 +78,13 @@ namespace Sudoku.Solver
 				{
 					// expectd to be an option
 					if (str.StartsWith("-r")) { OptRule = true; }
+					else if (str.StartsWith("-bo")) { OptBackTrack = true; OptBackTrackOnly = true; }
 					else if (str.StartsWith("-b")) { OptBackTrack = true; }
 					else if (str.StartsWith("-m")) { OptShowMask = true; }
 					else if (str.StartsWith("-fr")) { OptReadFileMultipleRow = true; }
 					else if (str.StartsWith("-f")) { OptReadFile = true; }
 					else if (str.StartsWith("-v")) { OptShowVertical = true; }
+					else if (str.StartsWith("-cp")) { OptCreateSudoku = true; OptCreateSudokuPuzzle = true; }
 					else if (str.StartsWith("-c")) { OptCreateSudoku = true; }
 					else if (str.StartsWith("-1")) { puzzlefile = demopuzzle1; }
 					else if (str.StartsWith("-2")) { puzzlefile = demopuzzle2; }
@@ -97,8 +103,12 @@ namespace Sudoku.Solver
 			if (count == 0) Console.WriteLine(MSGVersion);
 
 			if (OptCreateSudoku) {
-				CreateSudoku();
-				ShowPuzzle("New Random sudoku");
+				count = 1;
+				if (puzzlefile.Length > 0) { if (!Int32.TryParse(puzzlefile, out count)) count = 1; }
+				while (count-- > 0) {
+					CreateSudoku();
+					ShowPuzzle($"New Random sudoku {this.Puzzle.GetNumberCount()}");
+				}
 				return;
 			}
 
@@ -120,25 +130,8 @@ namespace Sudoku.Solver
 		}
 
 		SudokuPuzzle CreateSudoku() {
-			var newpuzzle = new CStr();
-			var rnd = new CRandom();
-			while (true) {
-				newpuzzle.Fill(81, (byte)'.');
-
-				newpuzzle.Set(0, (byte)('0' + rnd.RandomNumber(1, 9)));
-				newpuzzle.Set(1, (byte)('0' + rnd.RandomNumber(1, 9)));
-				newpuzzle.Set(2, (byte)('0' + rnd.RandomNumber(1, 9)));
-				
-				newpuzzle.Set(12, (byte)('0' + rnd.RandomNumber(1, 9)));
-				newpuzzle.Set(13, (byte)('0' + rnd.RandomNumber(1, 9)));
-				newpuzzle.Set(14, (byte)('0' + rnd.RandomNumber(1, 9)));
-
-				newpuzzle.Set(24, (byte)('0' + rnd.RandomNumber(1, 9)));
-				newpuzzle.Set(25, (byte)('0' + rnd.RandomNumber(1, 9)));
-				newpuzzle.Set(26, (byte)('0' + rnd.RandomNumber(1, 9)));
-
-				if (this.Puzzle.SetPuzzle(newpuzzle.ToString()).ResolveBacktrack()) break;
-			}
+			if (this.OptCreateSudokuPuzzle) this.Puzzle = new CreateSudoku().GetSudokuPuzzle();
+			else this.Puzzle = new CreateSudoku().GetNewSudoku();
 			return this.Puzzle;
 		}
 
@@ -206,23 +199,24 @@ namespace Sudoku.Solver
 			}
 
 			// Always start with rule based algorithm, if option rule is set then display progress
-			Console.WriteLine($"{this.Puzzle.GetPuzzle()}   - Starting 3R algorithm");
-			if (OptRule) {
-				while (Puzzle.ResolveRules(1) > 0) Console.WriteLine($"{this.Puzzle.GetPuzzle()}   - Resolved number");
-			} 
-			else Puzzle.ResolveRules();
-
-			if (this.Puzzle.IsSolved()) {
-				ShowPuzzle("Puzzle solved");
-				return this.Puzzle;
+			if (!OptBackTrackOnly) {
+				Console.WriteLine($"{this.Puzzle.GetPuzzle()}   - Starting 3R algorithm ({this.Puzzle.GetNumberCount()})");
+				if (OptRule) {
+					while (Puzzle.ResolveRules(1) > 0) Console.WriteLine($"{this.Puzzle.GetPuzzle()}   - Resolved number");
+				}
+				else Puzzle.ResolveRules();
+				if (this.Puzzle.IsSolved())	{
+					ShowPuzzle("Puzzle solved");
+					return this.Puzzle;
+				}
 			}
+
 
 			// Check if BackTrack should be used
 			if (OptBackTrack) {
-				Console.WriteLine($"{this.Puzzle.GetPuzzle()}   - Starting backtrack");
+				Console.WriteLine($"{this.Puzzle.GetPuzzle()}   - Starting backtrack ({this.Puzzle.GetNumberCount()})");
 				Puzzle.ResolveBacktrack();
-				if (this.Puzzle.IsSolved())
-				{
+				if (this.Puzzle.IsSolved()) {
 					ShowPuzzle("Puzzle solved");
 					return this.Puzzle;
 				}
@@ -302,10 +296,12 @@ namespace Sudoku.Solver
 		SudokuPuzzle Puzzle;
 		bool OptRule = false;
 		bool OptBackTrack = false;
+		bool OptBackTrackOnly = false;
 		bool OptShowMask = false;
 		bool OptReadFile = false;
 		bool OptReadFileMultipleRow = false;
 		bool OptShowVertical = false;
 		bool OptCreateSudoku = false;
+		bool OptCreateSudokuPuzzle = false;
 	}
 }
